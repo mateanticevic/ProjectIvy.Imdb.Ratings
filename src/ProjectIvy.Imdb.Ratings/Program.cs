cde76602287c2d0a8fb48e1a36c4da8b81e2f120
Mate Antičević <mate.anticevic@infobip.com>
2020-10-03 12:15:38 +0200

    Skip users without imdb cookies

--- src/ProjectIvy.Imdb.Ratings/Program.cs
+++ src/ProjectIvy.Imdb.Ratings/Program.cs
@@ -15,21 +15,28 @@ namespace ProjectIvy.Imdb.Ratings
 
         public static async Task Main(string[] args)
         {
-            _logger.Info("Application started");
+            LogInfo("Application started");
 
             try
             {
                 string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
                 var users = (await DbHandler.GetImdbUsers(connectionString)).ToList();
-                _logger.Info($"Found {users.Count} users with an Imdb account");
+                LogInfo($"Found {users.Count} users with an Imdb account");
 
                 foreach (var user in users)
                 {
-                    _logger.Info($"Processing movies for userId: {user.userId}");
+                    LogInfo($"Processing movies for userId: {user.userId}");
 
                     var existingIds = await DbHandler.GetMovieIds(connectionString, user.userId);
                     var imdbCookies = await DbHandler.GetImdbUserSecrets(connectionString, user.userId);
+
+                    if (imdbCookies.Count() == 0)
+                    {
+                        LogInfo($"User {user.userId} does not have imdb cookies");
+                        continue;
+                    }
+
                     var imdbRatings = await ImdbHandler.GetRatings(ImdbRatingsUrl, imdbCookies, user.imdbUsername);
 
                     var csvOptions = new CsvParserOptions(true, ',');
@@ -45,16 +52,28 @@ namespace ProjectIvy.Imdb.Ratings
                     {
                         newMovie.UserId = user.userId;
                         await DbHandler.InsertMovie(connectionString, newMovie);
-                        _logger.Info($"Movie: {newMovie.Title}, User: {newMovie.UserId}");
+                        LogInfo($"Movie: {newMovie.Title}, User: {newMovie.UserId}");
                     }
                 }
             }
             catch (Exception e)
             {
-                _logger.Error(e);
+                LogError(e.Message);
             }
 
-            _logger.Info("Application ended");
+            LogInfo("Application ended");
+        }
+
+        public static void LogInfo(string message)
+        {
+            _logger.Info(message);
+            Console.WriteLine(message);
+        }
+
+        public static void LogError(string message)
+        {
+            _logger.Error(message);
+            Console.WriteLine(message);
         }
     }
 }
